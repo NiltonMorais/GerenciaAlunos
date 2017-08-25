9c730e10426a764ef8c31e874fcf1f3452515c35
Nilton Morais <nilton_even@hotmail.com>
2017-08-25 16:08:10 -0300

    aula 06

---
+++ GerenciaUsuarios/Models/GerenciaUsuariosContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GerenciaUsuarios.Models
+{
+    public class GerenciaUsuariosContext: DbContext
+    {
+        public GerenciaUsuariosContext(): base("DefaultConnection")
+        {
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
